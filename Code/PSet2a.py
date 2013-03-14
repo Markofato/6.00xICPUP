a88f0360eb7833c6426cdc56d998d78c73bfd6d7
Mark Webber <mark.d.webber@gmail.com>
2013-03-14 23:21:14 +0800

    Finished PSet2a
    Passed both test cases with identicle results.
    Could neaten the code up.

--- Code/PSet2a.py
+++ Code/PSet2a.py
@@ -1,39 +1,24 @@
-balance = 500000
-annualInterestRate = 0.18
-monthlyPaymentRate = 0.02
-months = 0
-#expected tp = 1165.63 <3
-#expected ub = 4961.11 -> 4621.78
+balance = 4842
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
 
-#set initial values for b, r, p, tp
-b = balance
-r = annualInterestRate
-p = balance * monthlyPaymentRate
-#totalAmountPaid
-tp = 0
+b,r,p,m,tp = balance, annualInterestRate, balance * monthlyPaymentRate, 0, 0
 
-while months < 12:
-    #unpaidBalance
-    #if months == 11:
-       # m12ub = b
+while m < 12:
     ub = b - p
     #monthlyInterest
     i = (r/12.0) * ub
+    print("Months: {0}\nMinimum monthly payment: {1}".format(m,round(p,2)))
     tp += p
-    print(i)
-    print('Months: ' + str(months))
-    print('Minimum monthly payment: ' + str(p))
-    
     #outStandingBalance for next month
-    b =(ub + i)
-    if months != 11:
-        print('Remaining balance: ' + str(ub))
+    b = ub + i
+    if m != 11:
+        print('Remaining balance: {0}'.format(round(ub,2)))
     else:
-        print('Remaining balance: ' + str(b))
+        print('Remaining balance: {0}'.format(round(b,2)))
     #minimumAmountToPay for next month
-    p =b * monthlyPaymentRate
+    p = b * monthlyPaymentRate
+    m += 1
+print("Total Paid: {0}\nRemaining Balance: {1}".format(round(tp,2), round(b,2)))
     
-    months += 1
-    print('')
-print(tp)
-
+#Passed both TestCases.
